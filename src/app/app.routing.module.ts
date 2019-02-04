c52634320c53ee3bde820cf55a3bf9dfe75e54f1
unknown <leleovieira@gmail.com>
2019-02-04 20:50:25 -0200

    teste front end

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -40,12 +40,12 @@ const routes: Routes = [
     },
 
 
-    { path: '**', component: PublicComponent  },
+    { path: '**',  redirectTo: ''  },
 
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes,{useHash:true})],
     exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
